2f9dc5e90dcf89002d1d4744b8fca01e78d2c27b
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-12-08 00:01:22 +0100

    Add notes list and detail views

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from .models import Note
+
+
+def list(request):
+    return render(request, "notes/notes_list.html", {
+        "notes": Note.objects.all()
+    })
+
+
+def detail(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        raise Http404("Note doesn’t exist")
+
+    return render(request, "notes/notes_detail.html", {
+        "note": note
+    })
