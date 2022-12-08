745f9fb0173b855a54ad2af7d333bc3a7a2adf0f
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-12-08 15:52:58 +0100

    Add additional app

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home/welcome.html", {
+        "today": datetime.today()
+    })
