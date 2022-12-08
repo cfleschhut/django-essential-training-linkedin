5ddf395c47920d911844ce586dc013006e2af819
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-12-08 18:00:54 +0100

    Refactor notes app to class-based views

--- notes/views.py
+++ notes/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 from .models import Note
 
 
-def list(request):
-    return render(request, "notes/notes_list.html", {
-        "notes": Note.objects.all()
-    })
+class NoteListView(ListView):
+    model = Note
 
 
-def detail(request, pk):
-    try:
-        note = Note.objects.get(pk=pk)
-    except Note.DoesNotExist:
-        raise Http404("Note doesn’t exist")
-
-    return render(request, "notes/notes_detail.html", {
-        "note": note
-    })
+class NoteDetailView(DetailView):
+    model = Note
