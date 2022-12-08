605cc87579e16b6cdcc33ca152843d2141b9f379
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-12-08 16:02:47 +0100

    Add restricted page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, "home/welcome.html", {
         "today": datetime.today()
     })
+
+
+@login_required(login_url="/admin/")
+def authorized(request):
+    return render(request, "home/authorized.html")
