c5b29949e1515c9e3f76ec33adbdea2559cf7a95
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-12-09 11:44:09 +0100

    Namespace url names

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "notes"
+
 urlpatterns = [
-    path("notes/", views.NoteListView.as_view()),
-    path("notes/<int:pk>", views.NoteDetailView.as_view())
+    path("notes/", views.NoteListView.as_view(), name="list"),
+    path("notes/<int:pk>", views.NoteDetailView.as_view(), name="detail")
 ]
