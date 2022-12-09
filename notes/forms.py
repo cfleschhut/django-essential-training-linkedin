6637081ebc7f709eb1b8e2acb4293be7570dc5cb
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-12-09 18:28:17 +0100

    Customize label contents and form html element attributes

--- notes/forms.py
+++ notes/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 from django.core.exceptions import ValidationError
 
 from .models import Note
@@ -8,6 +8,13 @@ class NoteForm(ModelForm):
     class Meta:
         model = Note
         fields = ["title", "text"]
+        widgets = {
+            "title": TextInput(attrs={"class": "form-control"}),
+            "text": Textarea(attrs={"class": "form-control"})
+        }
+        labels = {
+            "text": "Write your thoughts here:"
+        }
 
     def clean_title(self):
         title = self.cleaned_data["title"]
