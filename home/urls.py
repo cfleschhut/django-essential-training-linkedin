3ae03b02f6bf61531eb7622863435cb4109f91bf
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-12-16 13:56:58 +0100

    Add login form view and logout button

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
 urlpatterns = [
-    path("home/", views.HomeView.as_view()),
-    path("authorized/", views.AuthorizedView.as_view())
+    path("", views.HomeView.as_view(), name="home"),
+    path("authorized/", views.AuthorizedView.as_view()),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout")
 ]
--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -130,4 +130,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_URL = "/admin/"
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "notes:list"
+LOGOUT_REDIRECT_URL = "home"
